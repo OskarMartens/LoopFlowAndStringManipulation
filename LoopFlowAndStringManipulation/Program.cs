a2a419760b436aff4ab14f4059587ac317be992c
OskarMartens <martens.oskar@gmail.com>
2024-05-21 15:38:52 +0200

    Add project files.

---
+++ LoopFlowAndStringManipulation/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace LoopFlowAndStringManipulation {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
