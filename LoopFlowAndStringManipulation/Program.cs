4be2b6d65ee12e32099e305b37cadc70b1185486
OskarMartens <martens.oskar@gmail.com>
2024-05-21 16:08:29 +0200

    Created the main menu of the application.

--- LoopFlowAndStringManipulation/Program.cs
+++ LoopFlowAndStringManipulation/Program.cs
@@ -1,7 +1,24 @@
 ﻿namespace LoopFlowAndStringManipulation {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            MainMenu();
+        }
+
+        static void MainMenu() {
+            Console.WriteLine("");
+            Console.WriteLine("Welcome to the main menu.");
+            Console.WriteLine("");
+            Console.WriteLine("This application consists of three different smaller applications.");
+            Console.WriteLine("The applications will be presented here in a numbered list.");
+            Console.WriteLine("In order to enter an application press the corresponding number in the console.");
+            Console.WriteLine("If you would like to exit the application, press \"0\".");
+            Console.WriteLine("Here are the applications to choose from:");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("1. Cinema application.");
+            Console.WriteLine("2. \"Repeat 10 times\".");
+            Console.WriteLine("3. \"The third word\".");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Please make your choice between 1, 2, 3 or 0 to exit.");
         }
     }
 }
